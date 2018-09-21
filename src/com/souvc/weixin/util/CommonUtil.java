c8d527e15cd1d40d33b46f71c5ab8858ae502edc
zhangtian <287839096@qq.com>
2018-09-21 17:05:53 +0800

    增加获取用户信息接口

--- src/com/souvc/weixin/util/CommonUtil.java
+++ src/com/souvc/weixin/util/CommonUtil.java
@@ -20,6 +20,7 @@ import org.slf4j.LoggerFactory;
 
 import com.souvc.weixin.menu.Menu;
 import com.souvc.weixin.pojo.Token;
+import com.souvc.weixin.pojo.WeixinUserInfo;
 
 /**
 * 类名: CommonUtil </br>
@@ -38,6 +39,10 @@ public class CommonUtil {
     // 菜单创建（POST） 限100（次/天）
     public static String menu_create_url =
     		"https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN";
+    
+    //请求用户详细信息
+    public static String user_info_get_url = 
+    		"https://api.weixin.qq.com/cgi-bin/user/info?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN";
     
     /**
      * 创建菜单
@@ -150,5 +155,59 @@ public class CommonUtil {
             }
         }
         return token;
+    }
+    
+    /**
+     * 获取用户信息
+     * 
+     * @param accessToken 接口访问凭证
+     * @param openId 用户标识
+     * @return WeixinUserInfo
+     */
+    public static WeixinUserInfo getUserInfo(String accessToken, String openId) {
+    	
+    	WeixinUserInfo weixinUserInfo = null;
+    	
+    	// 拼接请求地址
+    	String url = user_info_get_url.replace("ACCESS_TOKEN", accessToken).replace("OPENID", openId);
+    	
+    	// 获取用户信息
+        JSONObject jsonObject = CommonUtil.httpsRequest(url, "GET", null);
+        
+        if (null != jsonObject) {
+            try {
+                weixinUserInfo = new WeixinUserInfo();
+                // 用户的标识
+                weixinUserInfo.setOpenId(jsonObject.getString("openid"));
+                // 关注状态（1是关注，0是未关注），未关注时获取不到其余信息
+                weixinUserInfo.setSubscribe(jsonObject.getInt("subscribe"));
+                // 用户关注时间
+                weixinUserInfo.setSubscribeTime(jsonObject.getString("subscribe_time"));
+                // 昵称
+                weixinUserInfo.setNickname(jsonObject.getString("nickname"));
+                // 用户的性别（1是男性，2是女性，0是未知）
+                weixinUserInfo.setSex(jsonObject.getInt("sex"));
+                // 用户所在国家
+                weixinUserInfo.setCountry(jsonObject.getString("country"));
+                // 用户所在省份
+                weixinUserInfo.setProvince(jsonObject.getString("province"));
+                // 用户所在城市
+                weixinUserInfo.setCity(jsonObject.getString("city"));
+                // 用户的语言，简体中文为zh_CN
+                weixinUserInfo.setLanguage(jsonObject.getString("language"));
+                // 用户头像
+                weixinUserInfo.setHeadImgUrl(jsonObject.getString("headimgurl"));
+            } catch (Exception e) {
+                if (0 == weixinUserInfo.getSubscribe()) {
+                    log.error("用户{}已取消关注", weixinUserInfo.getOpenId());
+                } else {
+                    int errorCode = jsonObject.getInt("errcode");
+                    String errorMsg = jsonObject.getString("errmsg");
+                    log.error("获取用户信息失败 errcode:{} errmsg:{}", errorCode, errorMsg);
+                }
+            }
+        }
+        
+        return weixinUserInfo;
     }
 }
